b97201d63a4a11ee78b2bfda9fb96c066412823e
Matt Fasano <matt.fasano88@gmail.com>
2021-03-19 10:56:52 -0400

    update server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-require('dotenv').config()
 const PORT = 3000;
 const helmet = require("helmet");
 const cors = require("cors");
 const sgMail = require("@sendgrid/mail");
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 app.use(helmet());
 
